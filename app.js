2f6c3f71b011c5d58231ef60b444044339eb39a6
Anurag Kumar <anuragsingh4512@gmail.com>
2021-08-06 01:45:35 +0530

    js done

---
+++ app.js
@@ -0,0 +1,27 @@
+var inputAngle1 = document.querySelector("#input-angle1")
+var inputAngle2 = document.querySelector("#input-angle2")
+var inputAngle3 = document.querySelector("#input-angle3")
+
+var checkBtn = document.querySelector(".check")
+var divOutput = document.querySelector("#output");
+
+console.log(inputAngle1)
+console.log(inputAngle2)
+console.log(inputAngle3)
+console.log(checkBtn)
+
+
+function checkTriangleSum(){
+    
+    var triangleSum = Number(inputAngle1.value) + Number(inputAngle2.value)
+     + Number(inputAngle3.value);
+    if(triangleSum === 180){
+        divOutput.innerText = "Wohooo! These angle will make a triangle🔺"
+    }
+    else {
+        divOutput.innerText = "oh no! These angle didn't make a triangle😞" 
+    }
+    console.log(triangleSum)
+}
+
+checkBtn.addEventListener('click', checkTriangleSum)
