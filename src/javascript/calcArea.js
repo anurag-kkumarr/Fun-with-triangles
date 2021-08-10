f9ef6f9358f88e27bb4c126dd9410c3086259838
Anurag Kumar <anuragsingh4512@gmail.com>
2021-08-10 11:32:37 +0530

    js updated

--- src/javascript/calcArea.js
+++ src/javascript/calcArea.js
@@ -1,21 +1,29 @@
-const calArea = document.querySelectorAll(".calculateArea")
+const calArea = document.querySelectorAll(".calculateArea");
 //console.log(calArea)
 const calAreaBlock = document.querySelectorAll(".calculateAreaBlock");
+const showArea = document.querySelectorAll(".show-area");
+
+const inputOption1 = document.querySelectorAll(".input-option1");
+console.log(inputOption1);
+const inputOption2 = document.querySelectorAll(".input-option2");
+console.log(inputOption2);
+const inputOption3 = document.querySelector(".input-option3")
+const areaBtn1 = document.querySelector("#area-btn1");
+console.log(areaBtn1);
 
 const options = document.forms[0].elements;
 console.log(options)
 
-options[0].addEventListener('change',showOrHideBlock(calAreaBlock, 0, 1, 2, 3)
+options[0].addEventListener('change',(e)=>{
+    e.preventDefault();
+    showOrHideBlock(calAreaBlock, 0, 1, 2, 3);
+}
 )
-options[1].addEventListener('change',showOrHideBlock(calAreaBlock, 1, 0, 2, 3))
+options[1].addEventListener('change', ()=>{showOrHideBlock(calAreaBlock, 1, 0, 2, 3)})
 
-options[1].addEventListener('change',showOrHideBlock(calAreaBlock, 1, 0, 2, 3))
+options[2].addEventListener('change',()=>{showOrHideBlock(calAreaBlock, 2, 0, 1, 3)})
 
-options[1].addEventListener('change',showOrHideBlock(calAreaBlock, 1, 0, 2, 3))
-
-options[1].addEventListener('change',showOrHideBlock(calAreaBlock, 1, 0, 2, 3))
-
-
+options[3].addEventListener('change',()=>{showOrHideBlock(calAreaBlock, 3, 0, 1, 2)})
 
 
 
@@ -25,4 +33,12 @@ function showOrHideBlock(block, showIndex1, hidIndex1, hidIndex2, hidIndex3){
     block[hidIndex2].style.display = "none"
     block[hidIndex3].style.display = "none"
 }
+
+areaBtn1.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log("clicked");
+    const baseLength = Number(inputOption1[0].value);
+    const heightLength = Number(inputOption2[0].value);
+    showArea[0].innerText = (baseLength*heightLength)/2;
+})
 
