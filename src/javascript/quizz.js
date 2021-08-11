a236a3b747c8ef9619d25ad9030ee897fa5048f3
Anurag Kumar <anuragsingh4512@gmail.com>
2021-08-11 10:48:39 +0530

    quiz html done

--- src/javascript/quizz.js
+++ src/javascript/quizz.js
@@ -2,14 +2,27 @@ const quizForm = document.querySelector(".quiz-form");
 const checkBtn = document.querySelector("#check-btn");
 const outputDiv = document.querySelector("#output");
 
+console.log(quizForm);
+console.log(checkBtn);
+console.log(outputDiv);
+
 const correctAnswers = ["30°", "Isosceles triangle", "Equlateral triangle", "√3/4*a²", "30cm", "acute-angled", "3", "180°", "60°", "80°"];
 
 
 function checkScore(){
+    console.log("clicked");
     let score = 0;
     let index = 0;
     const formResults = new FormData(quizForm);
     console.log(formResults);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    outputDiv.innerText = "Your score is " + score;
+    //console.log(score);
 }
 
 checkBtn.addEventListener('click', checkScore);
